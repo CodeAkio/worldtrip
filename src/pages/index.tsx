c88d89cc760191757855dcff4e0642ea7a8ca142
CodeAkio <siqueira_2004@hotmail.com>
2021-08-10 16:26:29 -0300

    Add header to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 
 import { Banner } from "../components/Banner";
 import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
+import { Separator } from "../components/Separator";
 
 export default function Home() {
   return (
@@ -10,6 +11,18 @@ export default function Home() {
       <Header />
       <Banner />
       <Categories />
+
+      <Separator />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa?<br />
+        Então escolha seu continente
+      </Heading>
     </Flex>
   )
 }
