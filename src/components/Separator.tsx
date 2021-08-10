4e31a430d6b2ea428e0aa278b870c1a78de614a5
CodeAkio <siqueira_2004@hotmail.com>
2021-08-10 16:26:07 -0300

    Add separator do home page

---
+++ src/components/Separator.tsx
@@ -0,0 +1,7 @@
+import { Divider } from "@chakra-ui/react";
+
+export function Separator() {
+  return (
+    <Divider w={["60px", "90px"]} mx="auto" borderWidth="1px" borderColor="gray.700" my={["9", "20"]} />
+  )
+}
